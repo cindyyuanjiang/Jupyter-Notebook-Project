4b5f8c1d9bca17f03acd18dfd906c687e0129bed
yjiang2cmu <yjiang2@andrew.cmu.edu>
2022-06-16 01:03:50 -0400

    cleaned up generating python files and json dictionaries scripts

--- generate_json_dictionaries.py
+++ generate_json_dictionaries.py
@@ -1,25 +1,30 @@
 import os
 import sys
 import time
-from subprocess import call
 
 directory_in_str = sys.argv[1]
-directory = os.fsencode(directory_in_str)
-
+start = int(sys.argv[2])
+end = int(sys.argv[3])
+# directory = os.fsencode(directory_in_str)
 num_python_files = 0
 
-start = time.time()
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    if filename.endswith(".py"):
-        num_python_files += 1
-        print(directory_in_str)
-        print(filename)
-        print("\n\n\n")
-        filename = filename.replace(" ", "\ ")
-        command = "/Users/cindyjiang/Desktop/pyright/packages/pyright/index.js --lib " + directory_in_str + filename
-        os.system(command)
-        # call(command)
+start_time = time.time()
+# for file in os.listdir(directory):
+#     filename = os.fsdecode(file)
+#     if filename.endswith("_no_comments.py"):
+#         num_python_files += 1
+#         # print(directory_in_str)
+#         # print(filename)
+#         # print("\n\n\n")
+#         # filename = filename.replace(" ", "\ ")
+#         command = "/Users/cindyjiang/Desktop/pyright/packages/pyright/index.js --lib " + directory_in_str + filename
+#         os.system(command)
+for i in range(start, end + 1):
+    filename = "nb_" + str(i) + "_no_comments.py"
+    num_python_files += 1
+    command = "/Users/cindyjiang/Desktop/pyright/packages/pyright/index.js --lib " + directory_in_str + filename
+    os.system(command)
+end_time = time.time()
 
-print(num_python_files)
-print("time used = " + str(time.time() - start))
+print("Total number of python files analyzed = {}!".format(num_python_files))
+print("Time used = {} s!".format(end_time - start_time))
--- generate_python_files_from_nbs.js
+++ generate_python_files_from_nbs.js
@@ -1,59 +1,52 @@
-// var dep = require("./cell_deps.js");
 var line_dep = require("./line_deps.js");
-// var sk = require("./extract_sk_stmts.js");
-// var counting = require("./count_labels.js");
 var fs = require('fs');
 
 const args = process.argv.slice(2);
 const path = args[0];
+let num_notebooks = 0;
 
 function getExt(filename){
     return filename.substring(filename.lastIndexOf('.')+1, filename.length);
 }
 
-let num_notebooks = 0;
-
+const start = Date.now();
 fs.readdirSync(path).forEach(file => {
     if (getExt(file) === "ipynb") {
-        num_notebooks += 1;
-        console.log('Currently processing:');
-        console.log(path + file + '\n');
-
         let name = path + file;
-        const programSrc = fs.readFileSync(name).toString();
-        const programJson = JSON.parse(programSrc);
-
-        if (programJson.cells == undefined) {
-            var new_name = name.substring(0, name.lastIndexOf('.'));
-            fs.writeFile(new_name + '.py', "", function (err) {
-              if (err) throw err;
-            });
-            return;
-        }
-
-        var notebookCode = "";
-        for (let cell of programJson.cells) {
-            if (cell.cell_type === 'code') {
-                var sourceCode = "";
-                for (let line of cell.source) {
-                    if (line[0] != '#' && line[0] != '%' && line[0] != '!') {
-                        sourceCode += line;
+        var new_name = name.substring(0, name.lastIndexOf('.'));
+        num_notebooks += 1;
+        try{
+            const programSrc = fs.readFileSync(name).toString();
+            const programJson = JSON.parse(programSrc);
+            var notebookCode = "";
+            for (let cell of programJson.cells) {
+                if (cell.cell_type === 'code') {
+                    var sourceCode = "";
+                    for (let line of cell.source) {
+                        if (line[0] != '#' && line[0] != '%' && line[0] != '!') {
+                            sourceCode += line;
+                        }
+                    }
+                    if (sourceCode != "" && sourceCode[sourceCode.length -1] != "\n") {
+                        notebookCode += sourceCode + '\n';
+                    } else {
+                        notebookCode += sourceCode;
                     }
                 }
-                if (sourceCode != "" && sourceCode[sourceCode.length -1] != "\n") {
-                    notebookCode += sourceCode + '\n';
-                } else {
-                    notebookCode += sourceCode;
-                }
             }
-        }
-        if (notebookCode != "") {
-            var new_name = name.substring(0, name.lastIndexOf('.'));
             fs.writeFile(new_name + '_no_comments.py', notebookCode, function (err) {
+              if (err) throw err;
+            });
+        } catch (err) {
+            // comment out for measuring performance
+            // console.log("Error occured: " + err.message)
+            fs.writeFile(new_name + '_no_comments.py', "", function (err) {
               if (err) throw err;
             });
         }
     }
 });
 
-console.log(num_notebooks);
+const millis = Date.now() - start;
+console.log("Finished generating " + num_notebooks + " python files from notebooks!");
+console.log("Time used = " + millis + " ms!");
